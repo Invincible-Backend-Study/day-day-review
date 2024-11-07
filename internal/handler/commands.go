ed9e107c2ac3b33037625a5da59fcadb9952b1be
KyungMin Lee <hello.tidavid@gmail.com>
2024-11-07 20:48:18 +0900

    feat: Implemented command for discord bot

--- internal/handler/commands.go
+++ internal/handler/commands.go
@@ -4,9 +4,11 @@ import "github.com/bwmarrin/discordgo"
 
 // 명령어 상수
 const (
-	commandRegisterUser       = "회원-등록"
-	commandRegisterTodayScrum = "오늘-다짐"
-	commandGetTodayScrums     = "오늘-다짐-보기"
+	commandRegisterUser               = "회원-등록"
+	commandRegisterTodayScrum         = "오늘-다짐"
+	commandGetTodayScrums             = "오늘-다짐-보기"
+	commandRegisterTodayRetrospection = "오늘-회고"
+	commandGetTodayRetrospectives     = "오늘-회고-보기"
 )
 
 const (
@@ -18,6 +20,12 @@ const (
 	cIdRegisterScrumCommitmentInput = "scrum_commitment_input"
 	cIdRegisterScrumScoreInput      = "scrum_score_input"
 	cIdRegisterScrumReasonInput     = "scrum_reason_input"
+
+	cIdRegisterRetrospectionModal             = "retrospection_modal"
+	cIdRegisterRetrospectionGoalAchievedInput = "retrospection_goal_achieved_input"
+	cIdRegisterRetrospectionLearnedInput      = "retrospection_learned_input"
+	cIdRegisterRetrospectionScoreInput        = "retrospection_score_input"
+	cIdRegisterRetrospectionReasonInput       = "retrospection_reason_input"
 )
 
 // 명령어 목록
@@ -31,7 +39,15 @@ var commands = []*discordgo.ApplicationCommand{
 		Description: "오늘의 다짐을 등록합니다",
 	},
 	{
+		Name:        commandRegisterTodayRetrospection,
+		Description: "오늘의 회고를 등록합니다.",
+	},
+	{
 		Name:        commandGetTodayScrums,
 		Description: "오늘의 다짐을 모두 보여줍니다",
+	},
+	{
+		Name:        commandGetTodayRetrospectives,
+		Description: "오늘의 회고를 모두 보여줍니다.",
 	},
 }
